cb7acf344b5a8ba3fc8bafbc988f633c13a5aff0
David Mazza <github@dmzza.com>
2013-04-02 14:49:35 -0400

    Initial Commit

---
+++ NapTheLTests/NapTheLTests.h
@@ -0,0 +1,13 @@
+//
+//  NapTheLTests.h
+//  NapTheLTests
+//
+//  Created by David Mazza on 4/2/13.
+//  Copyright (c) 2013 David Mazza. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NapTheLTests : SenTestCase
+
+@end
---
+++ NapTheLTests/NapTheLTests.m
@@ -0,0 +1,32 @@
+//
+//  NapTheLTests.m
+//  NapTheLTests
+//
+//  Created by David Mazza on 4/2/13.
+//  Copyright (c) 2013 David Mazza. All rights reserved.
+//
+
+#import "NapTheLTests.h"
+
+@implementation NapTheLTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NapTheLTests");
+}
+
+@end
