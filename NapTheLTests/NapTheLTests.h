00108d588b1fa1fbf4846d4a76bdc4909c8ce957
David Mazza <github@dmzza.com>
2014-10-14 00:00:18 -0700

    Upgraded to XCTest and iOS 7.0

--- NapTheLTests/NapTheLTests.h
+++ NapTheLTests/NapTheLTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 David Mazza. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface NapTheLTests : SenTestCase
+@interface NapTheLTests : XCTestCase
 
 @end
--- NapTheLTests/NapTheLTests.m
+++ NapTheLTests/NapTheLTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in NapTheLTests");
+    XCTFail(@"Unit tests are not implemented yet in NapTheLTests");
 }
 
 @end
