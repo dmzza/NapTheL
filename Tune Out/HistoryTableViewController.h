ac318caa99b7affccb3768506d6f6bee3c75abff
David Mazza <github@dmzza.com>
2014-11-02 23:59:03 -0800

    History VC

---
+++ Tune Out/HistoryTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  HistoryTableViewController.h
+//  Tune Out
+//
+//  Created by David Mazza on 11/2/14.
+//  Copyright (c) 2014 Peaking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *trips;
+
+@end
